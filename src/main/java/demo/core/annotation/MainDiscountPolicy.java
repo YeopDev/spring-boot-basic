93ae6b946d2c4e5263e22e972d39beb7a26922e5
sangyeop <yeop1.dev@gmail.com>
2023-08-28 22:32:55 +0900

    :sparkles: feat(MainDiscountPolicy): 어노테이션 커스텀 - 메인 할인방법

---
+++ src/main/java/demo/core/annotation/MainDiscountPolicy.java
@@ -0,0 +1,14 @@
+package demo.core.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@Qualifier("mainDiscountPolicy")
+public @interface MainDiscountPolicy {
+// 어노테이션 만들어서 하는 방법도 좋은것 같다.
+}
