d8147ade5087b4b79a511334820e691c444fc3d3
sangyeop <yeop1.dev@gmail.com>
2023-09-03 00:55:58 +0900

    :white_check_mark: feat(BeanLifeCycleTest): 빈 라이프사이클 테스트

---
+++ src/test/java/demo/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,29 @@
+package demo.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest(){
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+//        @Bean(initMethod = "init", destroyMethod = "close") // destroyMethod = 자동으로 "close, shutdown" 을 추론해서 호출해준다. 외부라이브러리 초기화 , 종료해야할때 만 사용하자
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
